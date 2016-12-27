50816f3cc15f500ecedee15db03659993d49a1f7
Dale <dalema@umich.edu>
2016-12-27 16:28:09 -0500

    Finished the custom mode for two players

--- connect.cpp
+++ connect.cpp
@@ -14,6 +14,7 @@ using namespace std;
 
 void printHelpInfo();
 void simple();
+void custom();
 
 int main(int argc, char* argv[]){
     
@@ -24,7 +25,7 @@ int main(int argc, char* argv[]){
     }; // longOpts
     
     int option, index;
-    int mode;
+    int mode = 's';
     
     while ((option = getopt_long(argc, argv, "m:h", longOpts, &index)) != -1){
         switch (option) {
@@ -48,7 +49,10 @@ int main(int argc, char* argv[]){
             // run simple mode with 2 human players on a 6x7 board.
             simple();
             break;
-            
+        case 'c':
+            custom();
+            // run custom mode with 2 human players on a custom board.
+            break;
         default:
             cout << "Invalid mode selected!";
             printHelpInfo(); 
@@ -68,7 +72,9 @@ void printHelpInfo(){
     cout << "-h, --help: get helpful information about the game\n";
     cout << "-m, --mode <MODE>: MODE is optional. If unspecified will default to SIMPLE.\n";
     cout << "MODE option:\n";
-    cout << "   S: simple mode. two players will vs eachother on a standard 6x7 board.\n";
+    cout << "   s: simple mode. two players will vs eachother on a standard 6x7 board.\n";
+    cout << "   c: custom mode. two players will play eachother on a board the size of which they choose.\n";
+    cout << "      Players will also get to chose what piece they use throught the game.\n";
     
     return;
 }
@@ -96,6 +102,59 @@ void simple(){
     
     game.playGame();
     cout << "\nThanks for playing!";
+    return;
+}
+
+void custom() {
+    
+    string playerA_name;
+    string playerB_name;
+    char   playerA_piece;
+    char   playerB_piece;
+    int numRows = 0;
+    int numCols = 0;
+    int score;
+    
+    cout << "Custom mode is a two player mode with two human players.\n\n";
+    cout << "Player 1, please enter the name you'd like to use:";
+    getline(cin, playerA_name);
+    cout << playerA_name << " please select the piece you would like to use.\n";
+    cout << "Please select only one character: ";
+    cin >> playerA_piece;
+    cout << "\nPlayer2, please enter the name you'd like to use:";
+    cin.ignore();
+    //getline(cin, playerB_name);
+    getline(cin, playerB_name);
+    cout << "\n" << playerB_name << " please select the piece you would like to use.\n";
+    cout << "Please select only one character: ";
+    cin >> playerB_piece;
+    
+    cout << "\nPlease enter the dimensions of the board you'd like to use.\n";
+    cout << "Use the format numRows numCols (notice the space inbetween): ";
+    cin >> numRows >> numCols;
+    
+    while (numRows < 6){
+        cout << "\nThe number of rows you entered was invalid.\n";
+        cout << "Please choose a number greater than or equal to 6: ";
+        cin >> numRows;
+    }
+    
+    while (numCols < 7){
+        cout << "\nThe number of columns you entered was invalid.\n";
+        cout << "Please choose a number greater than or equal to 7: ";
+        cin >> numCols;
+    }
+    
+    
+    cout << "\nDiscuss amongst yourselves to what score you'd like to play to.\n";
+    cout << "What score would you like to play to? ";
+    cin >> score;
+    
+    cout << "\nThank you for you patience and good luck to both of you! Enjoy!\n\n";
+    
+    Game game(playerA_piece, playerA_name, playerB_piece, playerB_name, numRows, numCols, score);
+    
+    game.playGame();
     return;
 }
 
