e56eb90d28a1a6285c45e27822be958c53fdb240
Dale <dalema@umich.edu>
2017-01-22 21:56:49 -0500

    finished up player files for a *very* simple computer opponent.

--- player.cpp
+++ player.cpp
@@ -5,11 +5,76 @@
  */
 
 #include "player.h"
-using namespace std;
 
+Player* player_factory(std::string playerType_in, std::string name_in, char piece_in){
+    
+    if (playerType_in == "human"){
+        return new Human(name_in, piece_in);
+    } else if (playerType_in == "simple"){
+        return new Simple();
+    }
+    
+    return nullptr;
+}
+/******************************************************************************
+ IMPLEMENTATION OF PLAYER CLASS
+ ******************************************************************************/
 Player::Player():
-    name("John Doe"), piece('$'){}
+name("John Doe"), piece('$'){}
 
-Player::Player(string name_in, char piece_in):
-    name(name_in), piece(piece_in){}
+Player::Player(std::string name_in, char piece_in):
+name(name_in), piece(piece_in){}
 
+char Player::getPiece(){
+    return piece;
+}
+std::string Player::getName(){
+    return name;
+}
+/******************************************************************************
+ IMPLEMENTATION OF HUMAN CLASS
+ ******************************************************************************/
+
+Human::Human():
+Player(){}
+
+Human::Human(std::string name_in, char piece_in):
+Player(name_in, piece_in){}
+
+int Human::playPiece() {
+    int col;
+    
+    std::cout << getName() << " it's your turn.\n";
+    std::cout << "Please select which coloumn you'd like to play in: ";
+    std::cin >> col;
+    std::cout << "\n";
+    return col;
+}
+
+/******************************************************************************
+ IMPLEMENTATION OF SIMPLE CLASS
+ ******************************************************************************/
+
+Simple::Simple():
+Player(getRandomName(), getRandomPiece()){}
+
+Simple::Simple(std::string name_in, char piece_in):
+Player(name_in, piece_in){}
+
+int Simple::playPiece(){
+    // in the easyversion it will just chose a player at random.
+    // return number out of 1000.
+    return rand() % 1000;
+}
+
+char Simple::getRandomPiece(){
+    int index = rand() % NUMCHARS;
+    
+    return characters_for_computer[index];
+}
+
+std::string Simple::getRandomName(){
+    int index = rand() % NUMNAMES;
+    
+    return names_for_Computer[index];
+}
--- player.h
+++ player.h
@@ -50,7 +50,6 @@ class Player {
         virtual int playPiece() = 0;
 };
 
-
 class Human : public Player {
     public:
         Human();
@@ -74,86 +73,11 @@ private:
     std::string getRandomName();
 };
 
-/******************************************************************************/
-//MARK: Implementation
-
-
 /*
  *  REQUIRED: playerType_in must be all lowercase.
  */
 
-Player* player_factory(std::string playerType_in, std::string name_in, char piece_in){
-    
-    if (playerType_in == "human"){
-        return new Human(name_in, piece_in);
-    } else if (playerType_in == "simple"){
-        return new Simple();
-    }
-    
-    return nullptr;
-}
-/******************************************************************************
-                        IMPLEMENTATION OF PLAYER CLASS
- ******************************************************************************/
-Player::Player():
-name("John Doe"), piece('$'){}
-
-Player::Player(std::string name_in, char piece_in):
-name(name_in), piece(piece_in){}
-
-char Player::getPiece(){
-    return piece;
-}
-std::string Player::getName(){
-    return name;
-}
-/******************************************************************************
-                        IMPLEMENTATION OF HUMAN CLASS
- ******************************************************************************/
-
-Human::Human():
-    Player(){}
-
-Human::Human(std::string name_in, char piece_in):
-    Player(name_in, piece_in){}
-
-int Human::playPiece() {
-    int col;
-    
-    std::cout << getName() << " it's your turn.\n";
-    std::cout << "Please select which coloumn you'd like to play in: ";
-    std::cin >> col;
-    std::cout << "\n";
-    return col;
-}
-
-/******************************************************************************
-                        IMPLEMENTATION OF SIMPLE CLASS
- ******************************************************************************/
-
-Simple::Simple():
-    Player(getRandomName(), getRandomPiece()){}
-
-Simple::Simple(std::string name_in, char piece_in):
-    Player(name_in, piece_in){}
-
-int Simple::playPiece(){
-    // in the easyversion it will just chose a player at random.
-    // return number out of 1000.
-    return rand() % 1000;
-}
-
-char Simple::getRandomPiece(){
-    int index = rand() % NUMCHARS;
-    
-    return characters_for_computer[index];
-}
-
-std::string Simple::getRandomName(){
-    int index = rand() % NUMNAMES;
-    
-    return names_for_Computer[index];
-}
+Player* player_factory(std::string playerType_in, std::string name_in, char piece_in);
 
 #endif
 
